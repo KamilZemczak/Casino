c4f93d950a6609eb6ec86a1b3d24acf1bc879868
KamilZemczak <kamilzemb@gmail.com>
2016-10-29 11:13:08 +0200

    add welcome

---
+++ src/casino/Casino.java
@@ -0,0 +1,16 @@
+package casino;
+
+import java.util.Scanner;
+
+public class Casino {
+    public static Scanner input = new Scanner(System.in);
+    
+    public Casino() {
+        //******
+        //Wiadomość powitalna
+        //******
+        System.out.println("***************WITAJ W KASYNIE******************");
+    }
+
+   
+}
